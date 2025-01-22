f1fa37e6d147748edad711379c7c91d12c334888
April Hall <ari@arithefirst.com>
2025-01-22 00:57:00 -0500

    style: Prettier format

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,16 +3,16 @@ import bcrypt from 'bcrypt';
 
 export type HashType = 'md5' | 'sha256' | 'sha512' | 'bcrypt';
 export async function generateHash(input: string, hashType: HashType): Promise<string> {
-    switch (hashType) {
-        case 'md5':
-            return crypto.createHash('md5').update(input).digest('base64');
-        case 'sha256':
-            return crypto.createHash('sha256').update(input).digest('base64');
-        case 'sha512':
-            return crypto.createHash('sha512').update(input).digest('base64');
-        case 'bcrypt':
-            return Buffer.from(await bcrypt.hash(input, 10)).toString('base64');
-        default:
-            throw new Error('Invalid hash type');
-    }
-};
+  switch (hashType) {
+    case 'md5':
+      return crypto.createHash('md5').update(input).digest('base64');
+    case 'sha256':
+      return crypto.createHash('sha256').update(input).digest('base64');
+    case 'sha512':
+      return crypto.createHash('sha512').update(input).digest('base64');
+    case 'bcrypt':
+      return Buffer.from(await bcrypt.hash(input, 10)).toString('base64');
+    default:
+      throw new Error('Invalid hash type');
+  }
+}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,20 +3,21 @@ import { generateHash, type HashType } from '$lib';
 import { fail } from '@sveltejs/kit';
 
 export const actions = {
-	default: async ({ request }) => {
-		try {
-            const data = await request.formData();
-            const preHashedString = `${data.get('username')}@${data.get('website')}:${data.get('masterpassword')}`
-            const b64hash = await generateHash(preHashedString, (data.get('hashtype')! as HashType))
-            const charLimit = parseInt((data.get('characterlimit')! as string), 10)
-            if (charLimit == 0 || b64hash.slice(0,-1).length <= charLimit) {
-                return { success: true, password: b64hash.slice(0,-1) };
-            } return { success: true, password: b64hash.slice(0, charLimit) };
-        } catch (e) {
-            return fail(500, {
-                success: false,
-                e: e
-              });
-        }
-	}
+  default: async ({ request }) => {
+    try {
+      const data = await request.formData();
+      const preHashedString = `${data.get('username')}@${data.get('website')}:${data.get('masterpassword')}`;
+      const b64hash = await generateHash(preHashedString, data.get('hashtype')! as HashType);
+      const charLimit = parseInt(data.get('characterlimit')! as string, 10);
+      if (charLimit == 0 || b64hash.slice(0, -1).length <= charLimit) {
+        return { success: true, password: b64hash.slice(0, -1) };
+      }
+      return { success: true, password: b64hash.slice(0, charLimit) };
+    } catch (e) {
+      return fail(500, {
+        success: false,
+        e: e,
+      });
+    }
+  },
 } satisfies Actions;
