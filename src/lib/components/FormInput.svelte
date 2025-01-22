86d65c97efac4c888d3df3b269ee33ac362ac234
April Hall <ari@arithefirst.com>
2025-01-21 23:51:28 -0500

    feat: Create frontend

---
+++ src/lib/components/FormInput.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { createLabel, melt } from '@melt-ui/svelte';
+
+  const {
+    elements: { root },
+  } = createLabel();
+
+  interface props {
+    fieldName: string;
+    placeholder: string;
+    type: 'url' | 'text' | 'number' | 'password';
+  }
+
+  const { fieldName, placeholder, type }: props = $props();
+</script>
+
+<label use:melt={$root} for={fieldName.toLowerCase().replace(' ', '')} class="mb-0.5 mt-0.5 font-medium text-black" data-melt-part="root">
+  <span>{fieldName}</span>
+</label>
+<input
+  {type}
+  id={fieldName.toLowerCase().replace(' ', '')}
+  name={fieldName.toLowerCase().replace(' ', '')}
+  class="mb-0.5 h-10 w-[240px] rounded-md border-[1px] border-black bg-white px-3 py-2 text-black"
+  {placeholder}
+/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import FormInput from '$lib/components/FormInput.svelte';
+  import { enhance } from '$app/forms';
+</script>
+
+<main class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md border-2 border-black p-8">
+  <form method="POST" use:enhance>
+    <div class="flex flex-col items-start justify-center">
+      <FormInput type="url" fieldName="Website" placeholder="www.example.com" />
+      <FormInput type="text" fieldName="Username" placeholder="janedoe1970" />
+      <FormInput type="password" fieldName="Master Password" placeholder="password123" />
+      <FormInput type="number" fieldName="Character Limit" placeholder="16" />
+
+      <button
+        class="mx-auto mt-2 inline-flex items-center justify-center rounded-xl bg-black px-4 py-3
+        font-medium leading-none text-white shadow-md hover:opacity-75"
+      >
+        Show toast
+      </button>
+    </div>
+  </form>
+</main>
